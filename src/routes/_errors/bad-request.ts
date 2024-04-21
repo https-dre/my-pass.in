cf5f0278fccd10c2ce294136933d302db2e742e4
https-dre <diaso.andre@outlook.com>
2024-04-21 17:53:27 -0300

    feat: add statusNumber into BadRequest class allows passing an error status to the request

--- src/routes/_errors/bad-request.ts
+++ src/routes/_errors/bad-request.ts
@@ -1,3 +1,10 @@
 export class BadRequest extends Error {
-    
+    statusNumber: number = 400;
+
+    constructor(message: string, status?: number) {
+        super(message)
+        if (status) {
+            this.statusNumber = status;
+        }
+    }
 }
